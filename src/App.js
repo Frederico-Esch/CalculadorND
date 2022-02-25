b4f8ebd15c9fbe87a9d95d764dd5d9d7c7518479
Frederico <fredeschpe@gmail.com>
2022-02-24 21:21:00 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import MonsterInput from './components/MonsterInput';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="Header">
+        <h1>Calculador de ND</h1>
       </header>
+      <MonsterInput/>
     </div>
   );
 }
